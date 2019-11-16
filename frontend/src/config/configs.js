5e52b880fb04f544e69fd218ac0525676b78dcaf
amar.pai-fondekar <amar.pai-fondekar@socgen.com>
2019-11-16 19:31:54 +0530

    hosted in AWS

--- frontend/src/config/configs.js
+++ frontend/src/config/configs.js
@@ -1,6 +1,7 @@
 export const configurations = {
   countryApi: {
-    endpoint: "http://localhost:4000/api/v1/country/",
+    endpoint:
+      "http://ec2-3-85-238-57.compute-1.amazonaws.com:4000/api/v1/country/",
     errorMsg: "Someting went wrong... We're sorry, can you try again.",
     tableHeaders: [
       "Country Name",
@@ -13,7 +14,8 @@ export const configurations = {
     convertError: "Please select a country first"
   },
   jwtTokenApi: {
-    endpoint: "http://localhost:4000/api/v1/login",
+    endpoint:
+      "http://ec2-3-85-238-57.compute-1.amazonaws.com:4000/api/v1/login",
     errorMsg: "Someting went wrong while fetching token.."
   }
 };
